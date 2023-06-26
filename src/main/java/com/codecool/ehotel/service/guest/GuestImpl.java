8b6657242984bb2cd1f2c13281017cda88649aed
gaborati <113521931+gaborati@users.noreply.github.com>
2023-06-26 11:49:56 +0200

    task2

--- src/main/java/com/codecool/ehotel/service/guest/GuestImpl.java
+++ src/main/java/com/codecool/ehotel/service/guest/GuestImpl.java
@@ -3,6 +3,7 @@ package com.codecool.ehotel.service.guest;
 import com.codecool.ehotel.model.Guest;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -11,11 +12,22 @@ public class GuestImpl implements GuestService {
 
     @Override
     public Guest generateRandomGuest(LocalDate seasonStart, LocalDate seasonEnd) {
+
+
+
         return null;
     }
 
     @Override
     public Set<Guest> getGuestsForDay(List<Guest> guests, LocalDate date) {
-        return null;
+                   Set<Guest> guestsToday = new HashSet<>();
+
+                        for (Guest guest : guests) {
+                            if(guest.checkIn().isAfter(date) && guest.checkOut().isBefore(date) ){
+                                guestsToday.add(guest);
+                            }
+
+                        }
+        return guestsToday;
     }
 }
